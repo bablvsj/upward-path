1ea77c1a8a41f61b13638dcf6ac6da01a149bfb6
bablvsj <bablvsj@users.noreply.github.com>
2024-07-11 15:08:09 +0000

    deploy: bablvsj/upward-path-manage@9d8acb01f55dc20e8378c15a9d1e28280560b0da

--- assets/back_index.md.CdGpugEG.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o as a}from"./chunks/framework.CLn23qdP.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"back/index.md","filePath":"back/index.md","lastUpdated":1720691648000}'),n={name:"back/index.md"};function c(r,o,s,d,i,p){return a(),t("div")}const f=e(n,[["render",c]]);export{m as __pageData,f as default};
