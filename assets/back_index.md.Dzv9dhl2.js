59ca6f31870a31cf7848e0f513931ad34576d560
bablvsj <bablvsj@users.noreply.github.com>
2024-07-29 10:03:35 +0000

    deploy: bablvsj/upward-path-manage@79667fa88eeff1d4ddaf6ad502130c5f11803d5e

---
+++ assets/back_index.md.Dzv9dhl2.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a}from"./chunks/framework.6Vkfk6hO.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"back/index.md","filePath":"back/index.md","lastUpdated":1722247257000}'),n={name:"back/index.md"};function c(r,o,s,d,i,p){return a(),t("div")}const f=e(n,[["render",c]]);export{m as __pageData,f as default};
